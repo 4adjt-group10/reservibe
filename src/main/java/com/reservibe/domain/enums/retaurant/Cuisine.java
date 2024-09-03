faa74bd157c099890bbf09b6548e9ad71435dc27
Guilherme Lira <guilira.dev@gmail.com>
2024-09-03 20:07:23 -0300

    wip: modelagem pt2

---
+++ src/main/java/com/reservibe/domain/enums/retaurant/Cuisine.java
@@ -0,0 +1,21 @@
+package com.reservibe.domain.enums.retaurant;
+
+public enum Cuisine {
+    TURKISH,
+    ITALIAN,
+    MEXICAN,
+    CHINESE,
+    JAPANESE,
+    INDIAN,
+    FRENCH,
+    SPANISH,
+    GREEK,
+    THAI,
+    KOREAN,
+    VIETNAMESE,
+    GERMAN,
+    BRAZILIAN,
+    AMERICAN,
+    MEDITERRANEAN,
+    OTHER
+}
