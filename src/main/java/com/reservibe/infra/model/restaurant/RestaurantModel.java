570504e967d62321957d4c14aeb1443e63ece6d8
marcosMol <marcos_mol@hotmail.com>
2024-09-08 17:56:54 -0300

    feito alteração para salvar as horas de funcionamento do estabelecimento  em json no banco de dados

--- src/main/java/com/reservibe/infra/model/restaurant/RestaurantModel.java
+++ src/main/java/com/reservibe/infra/model/restaurant/RestaurantModel.java
@@ -2,11 +2,11 @@ package com.reservibe.infra.model.restaurant;
 
 import com.reservibe.domain.enums.retaurant.Cuisine;
 import com.reservibe.domain.entity.restaurant.OpeningHours;
-import io.swagger.v3.core.util.Json;
 import jakarta.persistence.*;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
